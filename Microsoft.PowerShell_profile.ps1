e394d2976eeebc12ea54f4222b4ffd2b4a40c176
Chris Gerke <103310@corrsnet.au>
2018-01-31 16:47:19 +0800

    Profile cleanup

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -30,19 +30,3 @@ if (Get-Command git -TotalCount 1 -ErrorAction SilentlyContinue) {
 # Everyone
 Write-Host "Execution Policy: " (Get-ExecutionPolicy)
 Write-Host "Profile : " $profile
-
-# GUI
-# $Host.UI.RawUI.WindowTitle = "Elevated"
-# $Host.UI.RawUI.BackgroundColor = 'Black'
-# $Host.UI.RawUI.ForegroundColor = 'White'
-# $Host.PrivateData.ErrorForegroundColor = 'Red'
-# $Host.PrivateData.ErrorBackgroundColor = $bckgrnd
-# $Host.PrivateData.WarningForegroundColor = 'Magenta'
-# $Host.PrivateData.WarningBackgroundColor = $bckgrnd
-# $Host.PrivateData.DebugForegroundColor = 'Yellow'
-# $Host.PrivateData.DebugBackgroundColor = $bckgrnd
-# $Host.PrivateData.VerboseForegroundColor = 'Green'
-# $Host.PrivateData.VerboseBackgroundColor = $bckgrnd
-# $Host.PrivateData.ProgressForegroundColor = 'Cyan'
-# $Host.PrivateData.ProgressBackgroundColor = $bckgrnd
-# Clear-Host
