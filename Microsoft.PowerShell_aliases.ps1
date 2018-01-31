1b1183d7bdd798c05122b4e06c9788caa213013c
Chris Gerke <103310@corrsnet.au>
2018-01-31 16:38:06 +0800

    Aliases

--- Microsoft.PowerShell_aliases.ps1
+++ Microsoft.PowerShell_aliases.ps1
@@ -1,4 +1,5 @@
 ${function:~} = { Set-Location ~ }
 ${function:Set-ParentLocation} = { Set-Location .. }; Set-Alias ".." Set-ParentLocation
 
+Set-Alias laps Get-Laps
 Set-Alias reload Reload-Powershell
