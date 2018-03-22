760770f5b20ab8f77e4774fcb29a03b301844ee9
Chris <chris.gerke@gmail.com>
2018-03-22 16:14:50 +0800

    Refactor

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,18 @@
 # Preferences
 $DebugPreference = "SilentlyContinue"
 
-<# Helper Functions #>
+<# Helpers #>
+
+# Log ALL THE THINGS! well some of them.
+Register-EngineEvent -SourceIdentifier PowerShell.Exiting -Action {
+    "PowerShell exited at {0}" -f (Get-Date) | 
+        Out-File -FilePath "C:\temp\powershell.log" -Append
+    Get-History | 
+        Out-File -FilePath "C:\temp\powershell.log" -Append
+}
 
 # Append paths to the env PATH
-function Append-Path([string] $path ) {
+function Set-EnvPath([string] $path ) {
     if ( -not [string]::IsNullOrEmpty($path) ) {
        if ( (Test-Path $path) -and (-not $env:PATH.contains($path)) ) {
            Write-Host "Appending Path" $path -ForegroundColor Cyan
@@ -13,128 +21,14 @@ function Append-Path([string] $path ) {
     }
  }
 
- # File sharing helpers
- function Enable-FPS {
-    netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=Yes
-}
-
-function Disable-FPS {
-    netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=No
-}
-
-# Report file/path length issues
+ # Report file/path length issues
 function Get-FilePathLength($path) {
     (Get-Childitem -LiteralPath $path -Recurse) | 
-    Where {$_.FullName.length -ge 248 } |
+    Where-Object {$_.FullName.length -ge 248 } |
     Format-Table -Wrap @{Label='Path length';Expression={$_.FullName.length}}, FullName
  }
 
-# LAPS password finder
-function Get-Laps {
-    param (
-        [parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]$ComputerObject
-    )
-    
-    try {
-        Get-ADComputer $ComputerObject -Properties ms-Mcs-AdmPwd | select name, ms-Mcs-AdmPwd
-    } catch {
-        return $false
-    }
-}
-
- # Wifi helpers
-function Get-WIFI($SSID) {
-    (netsh wlan show profiles)
-} 
-function Remove-WIFI($SSID) {
-   (netsh wlan delete profile name=$SSID)
-} 
-
- # Reload ps on-demand
- function Reload-Powershell {
-    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
-    [System.Diagnostics.Process]::Start($newProcess);
-    exit
-}
-
-# Reset offline files database
-function Reset-Offline {
-    Push-Location
-    Set-Location HKLM:
-    Set-ItemProperty ".\SYSTEM\CurrentControlSet\services\CSC\Parameters" -name "FormatDatabase" -Value 1 -PropertyType "DWord"
-    Pop-Location
-}
-
-# Reset windows search index
-function Reset-SearchIndex {
-    if (Test-Administrator) {
-        Stop-Service Wsearch
-        Start-Sleep -s 5
-        dir C:\ProgramData\Microsoft\Search\Data\Applications\Windows\
-        Rename-Item C:\ProgramData\Microsoft\Search\Data\Applications\Windows\Windows.edb Windows.edb.bak
-        dir C:\ProgramData\Microsoft\Search\Data\Applications\Windows\
-        Start-Sleep -s 5
-        Start-Service Wsearch
-        control /name Microsoft.IndexingOptions
-
-        #Alternate
-        #Push-Location
-        #Set-Location HKLM:
-        #Set-ItemProperty ".\SOFTWARE\Microsoft\Windows Search" -name "SetupCompletedSuccessfully" -Value 0 -PropertyType "DWord"
-        #Pop-Location
-
-    } else {
-        Write-Host "Must be elevated."
-    }
-}
-
-# Reset network stack
-function Reset-Stack {
-    if (Test-IsAdmin) {
-        ipconfig /flushdns
-        nbtstat -R
-        nbtstat -RR
-        netsh int ipv4 reset
-        netsh int ipv6 reset
-        netsh int ip reset
-        netsh int tcp reset
-        netsh winsock reset
-    } else {
-        Write-Host "Must be elevated."
-    }
-}
-
- # Set power profile
- function Set-Power {
-    # powercfg -L # to get the Available list of all Power Settings  schemes
-    # powercfg -GETACTIVESCHEME
-    #Existing Power Schemes (* Active)
-    #-----------------------------------
-    #Power Scheme GUID: 381b4222-f694-41f0-9685-ff5bb260df2e  (Balanced)
-    #Power Scheme GUID: 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c  (High performance) *
-    #Power Scheme GUID: a1841308-3541-4fab-bc81-f71556f20b4a  (Power saver)
-    $DesiredProfile = "Power Scheme GUID: 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c  (High performance)".Split()
-    $CurrentProfile = $(powercfg -GETACTIVESCHEME).Split()
-    if ($CurrentProfile[3] -ne $DesiredProfile[3]) {
-        powerCfg -SetActive $DesiredProfile[3]
-    }
-}
-
-# Setup openshh. Test this, make it smarter
-function Setup-SSH {
-    # Test pipe
-    #Get-WindowsCapability -Online | ? Name -like 'OpenSSH*' | Add-WindowsCapability -Online
-    Get-WindowsCapability -Online | ? Name -like 'OpenSSH*'
-
-    # Install the OpenSSH Client
-    Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
-
-    # Install the OpenSSH Server
-    # Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-}
-
-# Test elevation
+ # Test elevation
 function Test-IsAdmin {
     $user = [Security.Principal.WindowsIdentity]::GetCurrent();
     (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
@@ -177,12 +71,12 @@ function Test-RegistryValue {
 
 <# dot source #>
 Push-Location (Split-Path -parent $profile)
-"functions","aliases","organisation" | Where-Object {Test-Path "Microsoft.PowerShell_$_.ps1"} | ForEach-Object -process {
+"aliases","organisation","test" | Where-Object {Test-Path "Microsoft.PowerShell_$_.ps1"} | ForEach-Object -process {
     Invoke-Expression ". .\Microsoft.PowerShell_$_.ps1"; #Write-Host Microsoft.PowerShell_$_.ps1
 }
 Pop-Location
 
-<# Modules #>
+<# Get Modules #>
 $ProfilePath = split-path -parent $PROFILE
 #$Modules = @("posh-git", "PSPath", "PSSudo")
 if($Modules -ne $null) {
@@ -199,19 +93,7 @@ if($Modules -ne $null) {
     Set-PSReadlineOption -EditMode Emacs
 }
 
-<# Implicit Remoting
-Use tools without installing or modifying the local workstation by
-importing a session from a host that has them. Needs testing.
-#>
-function Get-ImplicitModule($endpoint, $module) {
-    $endpointsession=New-PSSession -ComputerName $endpoint
-    Import-PSSession -Session $endpointsession -Module $module
-}
-
 <# HUD #>
-
-
-# Profile
 Write-Host "Execution Policy: " (Get-ExecutionPolicy)
 Write-Host "Profile : " $profile
 
@@ -223,7 +105,7 @@ function prompt {
     if (Get-GitStatus){
         if (Get-Command git -TotalCount 1 -ErrorAction SilentlyContinue) {
             Write-Host (git --version) -ForegroundColor Cyan
-            Append-Path((Get-Item "Env:ProgramFiles").Value + "\Git\bin")
+            Set-EnvPath((Get-Item "Env:ProgramFiles").Value + "\Git\bin")
         }
     }
 
