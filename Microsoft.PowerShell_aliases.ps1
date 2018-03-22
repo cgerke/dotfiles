98381c7bef488d59fd4a1d636f295208b9260abb
Chris <chris.gerke@gmail.com>
2018-03-22 16:16:19 +0800

    Refactor

--- Microsoft.PowerShell_aliases.ps1
+++ Microsoft.PowerShell_aliases.ps1
@@ -2,4 +2,4 @@ ${function:~} = { Set-Location ~ }
 ${function:Set-ParentLocation} = { Set-Location .. }; Set-Alias ".." Set-ParentLocation
 
 Set-Alias laps Get-Laps
-Set-Alias reload Reload-Powershell
+Set-Alias reload Restart-Powershell
