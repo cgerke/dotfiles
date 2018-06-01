bdf6b6cba2910cbb4ec884f77405b637e745c843
Chris <chris.gerke@gmail.com>
2018-06-01 10:15:07 +0800

    cheat sheet
    html cheat sheet for modules

--- Microsoft.PowerShell_test.ps1
+++ Microsoft.PowerShell_test.ps1
@@ -1,4 +1,42 @@
 <# these should be modules dude, write em! #>
+function Get-CheatSheet {
+    param (
+        [Parameter(Mandatory=$true,HelpMessage="`t Get-Module -ListAvailable for parameters...")]
+	    [ValidateNotNullOrEmpty()]$ModuleName
+    )
+    
+    try {
+    # adjust the name of the module
+    # code will list all commands shipped by that module
+    # list of all modules: Get-Module -ListAvailable
+    # $ModuleName = "PrintManagement"
+    $Title = "$ModuleName Commands"
+    $OutFile = "$env:temp\commands.html"
+$StyleSheet = @"
+<title>$Title</title>
+<style>
+h1, th { text-align: center; font-family: Segoe UI; color:#0046c3;}
+table { margin: auto; font-family: Segoe UI; box-shadow: 10px 10px 5px #888; border: thin ridge grey; }
+th { background: #0046c3; color: #fff; max-width: 400px; padding: 5px 10px; }
+td { font-size: 11px; padding: 5px 20px; color: #000; }
+tr { background: #b8d1f3; }
+tr:nth-child(even) { background: #dae5f4; }
+tr:nth-child(odd) { background: #b8d1f3; }
+</style>
+"@
+    $Header = "<h1 align='center'>$title</h1>"
+    Get-Command -Module $moduleName | 
+    Get-Help | 
+    Select-Object -Property Name, Synopsis |
+    ConvertTo-Html -Title $Title -Head $StyleSheet -PreContent $Header |
+    Set-Content -Path $OutFile
+    Invoke-Item -Path $OutFile
+
+    } catch {
+        Get-Module -ListAvailable
+
+    }
+}
 
 function Reset-Google {
     If (Test-Path 'HKCU:\Software\Policies\Google') {
