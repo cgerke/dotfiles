cf57b0b9de9f0baec4a57b593c960aa521207115
Chris <chris.gerke@gmail.com>
2018-07-22 08:50:15 +0800

    Streamlining

--- Microsoft.PowerShell_aliases.ps1
+++
@@ -1,5 +0,0 @@
-${function:~} = { Set-Location ~ }
-${function:Set-ParentLocation} = { Set-Location .. }; Set-Alias ".." Set-ParentLocation
-
-Set-Alias laps Get-Laps
-Set-Alias reload Restart-Powershell
